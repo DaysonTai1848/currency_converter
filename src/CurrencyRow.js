5d3f2a07f525edb910392509d22385da0d5a8caa
DaysonTai1848 <97086571+DaysonTai1848@users.noreply.github.com>
2022-03-21 14:10:53 +0800

    Add files via upload

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -4,14 +4,23 @@ import "./CurrencyRow.css";
 const CurrencyRow = (props) => {
 
   // destructure / spread it into object 
-  const {currencyOptions} = props
+  const {
+    currencyOptions,
+    selectedCurrency,
+    onChangeCurrency,
+    amount,
+    onChangeAmount
+  } = props
 
   return (
     <div className='CurrencyRow'>
 
         {/* type="nubmer" ensures that user is only able to type in numbers */}
-        <input type="number" className='input-box'/>
-        <select className='drop-down'>
+        <input type="number" className='input-box' value={amount} onChange={onChangeAmount}/>
+        <select className='drop-down' value={selectedCurrency} onChange={onChangeCurrency}>
+
+          {/* this is a loop to populate all the currencies from API into the options */}
+          
           {currencyOptions.map(option =>(
             <option key={option} value={option}>{option}</option>
           ))}
